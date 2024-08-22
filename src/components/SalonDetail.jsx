00ca64130dc2c69af1a67924e06b8e8f48d94f79
Cactusjuice <96051596+salunkemanas@users.noreply.github.com>
2024-08-22 08:51:47 +0530

    Update SalonDetail.jsx

--- src/components/SalonDetail.jsx
+++ src/components/SalonDetail.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import Carousel from "../components/Carousel"
+import React from 'react';
+import Carousel from "../components/Carousel";
+
 const SalonDetail = () => {
 
   const handleClick = () =>{
     console.log("clicked")
   }  
+
   return (
-    <div className='container'>
-        <div className='text-center p-5'>
-            <div className='font-bold text-3xl'>Salon Details</div>
-            <div className='p-10 text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+    <div className='container mx-auto'>
+        <div className='flex flex-col items-center p-5'>
+            <div className='font-bold text-3xl text-center'>Salon Details</div>
+            <div className='p-10 text-xl text-center max-w-2xl'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                 Rem perferendis cum vitae expedita laudantium, iure facere sint? Iusto dicta reiciendis 
                 totam dolorem, natus quasi cumque ipsa numquam optio deserunt autem!
             </div>
             <Carousel/>
-            <button onClick={handleClick} className="mt-5 bg-blue-500 text-white p-2 rounded">
-            Book Appointment
+            <button 
+              onClick={handleClick} 
+              className="mt-5 bg-blue-500 text-white p-2 rounded"
+            >
+              Book Appointment
             </button>
         </div>
     </div>
   )
 }
 
-export default SalonDetail
+export default SalonDetail;
